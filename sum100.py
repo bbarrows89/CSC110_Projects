c5a4aaf7418b1031edf69b1e42681531f2c3adc2
Bryan Barrows <bryanbarrows@live.com>
2017-02-18 17:59:41 -0800

    Add files via upload
    one of my first loops... finds the sum of integers between 0-100.
    
    proposed functionality upgrade:  let user set the range of integers to add up.

---
+++ sum100.py
@@ -0,0 +1,13 @@
+# Bryan Barrows
+# CSC 110 9830 Assignment #1 Problem #3
+# 1/14/17
+# file: sum100.py
+# This script finds the sum of integers between 0-100
+
+def main():
+        sum = 0
+        for i in range(101):                      
+                sum = sum + i
+        print (sum)
+
+main()
