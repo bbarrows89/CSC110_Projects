c81ecdf74f3e668559ed4c257e3cdfb1d95f376c
Bryan Barrows <bryanbarrows@live.com>
2017-02-18 17:43:03 -0800

    Add files via upload
    myFirstPythonProgram.py      ---   this script takes an input between 0 and 1 and demonstrates a chaotic mathematical function...
    
    The first value is fed into a loop which performs a series of multiplication and keeps generating new, "random" and chaotic values.

---
+++ myFirstPythonProgram.py
@@ -0,0 +1,14 @@
+# Bryan Barrows
+# CSC 110 - 9830
+# January 13th, 2017
+# File: myFirstPythonProgram.py
+# A simple program illustrating chaotic behavior.
+
+def main():
+    print("This program illustrates a chaotic function")
+    x = eval(input("Enter a number between 0 and 1: "))
+    for i in range(10):
+        x = 3.9 * x * (1 - x)
+        print(x)
+
+main()
