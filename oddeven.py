d753761d0a25a779e157894386fcc56eded03a79
Bryan Barrows <bryanbarrows@live.com>
2017-02-27 15:13:19 -0800

    Add files via upload

---
+++ oddeven.py
@@ -0,0 +1,19 @@
+# Bryan Barrows
+# Week 11 Practice Exercises
+# CSC 110 - Winter 17
+# oddeven.py
+# A simple function to see if a number is even or odd.
+
+
+
+def main():
+
+    num = eval(input("Enter a number: "))
+
+    if (num % 2 == 0):
+        print("Your number is even")
+
+    else:
+        print("Your number is odd")
+
+main()
