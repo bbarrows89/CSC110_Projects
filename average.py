bc203db30a5ff429fd7c2bb02ac5197856e866d4
Bryan Barrows <bryanbarrows@live.com>
2017-03-05 11:54:16 -0800

    Add files via upload

---
+++ average.py
@@ -0,0 +1,17 @@
+# average.py
+# CSC 110 - Winter 2017
+# Bryan Barrows
+
+
+def main():
+    sum = 0.0
+    count = 0
+    xStr = input("Enter a number (<Enter> to quit): ")
+    while xStr != "":
+        x = eval(xStr)
+        sum = sum + x
+        count = count + 1
+        xStr = input ("Enter a number (<Enter> to quit): ")
+    print("\nThe average of the numbers is", sum / count)
+
+main()
